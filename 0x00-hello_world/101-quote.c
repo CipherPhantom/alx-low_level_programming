d8bf5aa10d81aae8917400e24decc6707e854b65
CipherPhantom <oyepitan10@gmail.com>
2023-02-16 18:43:31 +0100

    Fixed Errors - Advanced Tasks

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,9 +7,8 @@
 
 int main(void)
 {
-	char string[59];
+	char s[59] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	string = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	fwrite(string, 57, 1, stderr);
+	fwrite(s, 59, 1, stderr);
 	return (1);
 }
