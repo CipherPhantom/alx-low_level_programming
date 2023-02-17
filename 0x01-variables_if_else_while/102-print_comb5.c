0249b4eddf2b238774f25a9150745e48921b6acf
CipherPhantom <oyepitan10@gmail.com>
2023-02-17 15:31:59 +0100

    Code Refractoring

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,37 +9,24 @@ int main(void)
 {
 	int i = 0;
 
-	while (i < 10)
+	while (i < 100)
 	{
 		int j = 0;
 
-		while (j < 10)
+		while (j < 100)
 		{
-			int k = 0;
-
-			while (k < 10)
+			if (i < j)
 			{
-				int l = 0;
-
-				while (l < 10)
+				putchar(i / 10 + '0');
+				putchar(i % 10 + '0');
+				putchar(' ');
+				putchar(j / 10 + '0');
+				putchar(j % 10 + '0');
+				if (i != 98)
 				{
-					if (i * 10 + j < k * 10 + l)
-					{
-
-						putchar(i + '0');
-						putchar(j + '0');
-						putchar(' ');
-						putchar(k + '0');
-						putchar(l + '0');
-						if ((i * 10 + j) != 98)
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
-				l++;
+					putchar(',');
+					putchar(' ');
 				}
-			k++;
 			}
 		j++;
 		}
