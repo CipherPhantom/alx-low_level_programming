109b770ea2dc7c12cd6c075bc163c059f408ec35
CipherPhantom <oyepitan10@gmail.com>
2023-02-22 17:50:10 -0800

    0x03-debugging

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	unsigned long int n, m = 1, next_term = 1;
+	unsigned long int n = 0, m = 1, next_term = 1;
 	int i = 0;
 
 	while (i < 98)
@@ -17,6 +17,7 @@ int main(void)
 		n = m;
 		m = next_term;
 		next_term = n + m;
+		printf("%lu %lu %lu\n", n, m, next_term);
 		if (i < 97)
 			printf(", ");
 		i++;
