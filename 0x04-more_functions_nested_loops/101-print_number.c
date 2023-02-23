99a9acd38bd8381be5634b5e954d2c4fcacb5551
CipherPhantom <oyepitan10@gmail.com>
2023-02-23 15:12:08 -0800

    Fix Error 11

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,30 +7,32 @@
 
 void print_number(int n)
 {
-	if (n < 0)
+	int d = n;
+
+	if (d < 0)
 	{
 		_putchar('-');
-		n *= -1;
+		d *= -1;
 	}
 
-	if (n < 10)
-		_putchar(n + '0');
-	else if (n < 100)
+	if (d  < 10)
+		_putchar(d + '0');
+	else if (d < 100)
 	{
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
+		_putchar(d / 10 + '0');
+		_putchar(d % 10 + '0');
 	}
 	else if (n < 1000)
 	{
-		_putchar(n / 100 + '0');
-		_putchar((n - ((n / 100) * 100)) / 10 + '0');
-		_putchar(n % 10 + '0');
+		_putchar(d / 100 + '0');
+		_putchar((d - ((d / 100) * 100)) / 10 + '0');
+		_putchar(d % 10 + '0');
 	}
 	else
 	{
-		_putchar(n / 1000 + '0');
-		_putchar((n - ((n / 1000) * 1000)) / 100 + '0');
-		_putchar((n - ((n / 100) * 100)) / 10 + '0');
-		_putchar(n % 10 + '0');
+		_putchar(d / 1000 + '0');
+		_putchar((d - ((d / 1000) * 1000)) / 100 + '0');
+		_putchar((d - ((d / 100) * 100)) / 10 + '0');
+		_putchar(d % 10 + '0');
 	}
 }
