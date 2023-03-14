90eb53359092a27e519a7940bc3d6946e9fc6a26
CipherPhantom <oyepitan10@gmail.com>
2023-03-14 04:18:09 -0700

    0x0B-malloc_free-2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,7 +22,10 @@ char *str_concat(char *s1, char *s2)
 
 	len1 = strlen(s1);
 	len2 = strlen(s2);
-	s = (char *)malloc(sizeof(char) * (len1 + len2));
+	s = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (s == NULL)
+		return (NULL);
 
 	i = 0;
 	while (s1[i] != '\0' && len1)
@@ -38,6 +41,5 @@ char *str_concat(char *s1, char *s2)
 		i++;
 		j++;
 	}
-	s[i] = '\0';
 	return (s);
 }
