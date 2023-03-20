effb47878df2af1e5eaf2c10ca372cea006af194
Oyepitan Fortune <oyepitan10@gmail.com>
2023-03-20 16:49:53 +0100

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,12 +12,10 @@ int _strlen(char *s)
 	int i;
 
 	i = 0;
-
 	while (s[i] != '\0')
 	{
 		i++;
 	}
-
 	return (i);
 }
 
@@ -35,18 +33,15 @@ char *_strcpy(char *dest, char *src)
 	int len, i;
 
 	len = 0;
-
 	while (src[len] != '\0')
 	{
 		len++;
 	}
-
 	for (i = 0; i < len; i++)
 	{
 		dest[i] = src[i];
 	}
 	dest[i] = '\0';
-
 	return (dest);
 }
 
@@ -63,20 +58,20 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *dog;
 	int len1, len2;
 
-	len1 = _strlen(name);
-	len2 = _strlen(owner);
+	l1 = _strlen(name);
+	l2 = _strlen(owner);
 
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
 
-	dog->name = malloc(sizeof(char) * (len1 + 1));
+	dog->name = malloc(sizeof(char) * (l1 + 1));
 	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
-	dog->owner = malloc(sizeof(char) * (len2 + 1));
+	dog->owner = malloc(sizeof(char) * (l2 + 1));
 	if (dog->owner == NULL)
 	{
 		free(dog);
