b17c7e8f8d81060a3fccf8bac14497e905865ba5
CipherPhantom <oyepitan10@gmail.com>
2023-03-20 06:50:26 -0700

    2-print_dog.c -v3

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,15 +9,11 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	if (d->name == NULL || d->name[0] == '\0')
+	if (d->name == NULL)
 		d->name = "(nil)";
 	printf("Name: %s\n", d->name);
-	if (d->age <= 0.00)
-		printf("Age: (nil)\n");
-	else
-		printf("Age %f\n", d->age);
-
-	if (d->owner == NULL || d->owner[0] == '\0')
+	printf("Age %f\n", d->age);
+	if (d->owner == NULL)
 		d->owner = "(nil)";
 	printf("Owner: %s\n", d->owner);
 }
