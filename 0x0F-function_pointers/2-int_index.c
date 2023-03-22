cadedc6019fadef44fa3fed477fb11a6a3d86b55
CipherPhantom <oyepitan10@gmail.com>
2023-03-22 11:57:24 -0700

    Fix Error_2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,9 +15,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 		return (-1);
 	if (array != NULL && cmp != NULL)
 	{
-		for(i = 0; i < size; i++)
+		for (i = 0; i < size; i++)
 		{
-			if (cmp(array[i]))
+			if (cmp(array[i]) != 0)
 				return (i);
 		}
 	}
