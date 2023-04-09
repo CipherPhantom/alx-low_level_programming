682901c0fa7ae56c822e6a5b8eb79ff71c14cdbb
CipherPhantom <oyepitan10@gmail.com>
2023-04-09 00:42:57 -0700

    Fix Error_4

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,60 +17,42 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	
 	buffer = malloc(1024 * sizeof(char));
 	if (!buffer)
-		file_to_error(argv[2], buffer);
-
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		free(buffer);
+		exit(99);
+	}
 	o_from = open(argv[1], O_RDONLY);
 	o_to = creat(argv[2], 0664);
 	r_from = read(o_from, buffer, 1024);
-
 	do {
 		if (o_from == -1 || r_from == -1)
-			file_from_error(argv[1], buffer);
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			exit(98);
+		}
 
-		w_to = write(o_to, buffer, 1024);
+		w_to = write(o_to, buffer, r_from);
 		if (w_to == -1 || o_to == -1)
-			file_to_error(argv[2], buffer);
-
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
 		r_from = read(o_from, buffer, 1024);
 		o_to = open(argv[2], O_WRONLY | O_APPEND);
 	} while (r_from > 0);
-
 	free(buffer);
 	close_fd(o_from);
 	close_fd(o_to);
 	return (0);
 }
-
 /**
- * file_from_error - Prints error message(file_from) and exit 98
- * @s: Name of file.
- * @buf: Buffer
- */
-void file_from_error(char *s, char *buf)
-{
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", s);
-	free(buf);
-	exit(98);
-}
-
-/**
- * file_to_error - Prints error message(file_to) and exit 99
- * @s: Name of file.
- * @buf: Buffer
- */
-void file_to_error(char *s, char *buf)
-{
-	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", s);
-	free(buf);
-	exit(99);
-}
-
-/**
- * close_fd - Closes, prints error message if closing fails file and exit 100
- * @fd: File descriptor
+ * close_fd - Close a file
+ * @fd: file descriptor
  */
 void close_fd(int fd)
 {
