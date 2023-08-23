f5b68b9c10db2a2c1cc7a341dc4eef2c875b6b2b
Cipher Phantom <oyepitan10@gmail.com>
2023-08-23 23:06:18 +0100

    Advanced Task

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -6,15 +6,18 @@ def island_perimeter(grid):
     """Returns the perimeter of the island described in grid"""
 
     perimeter = 0
+    if 1 in grid[0] or 1 in grid[-1] or \
+            any(row[0] == 1 or row[-1] == 1 for row in grid):
+        return perimeter
     for i in range(len(grid)):
         for j in range(len(grid[0])):
-                if grid[i][j] == 1:
-                    if grid[i - 1][j] == 0:
-                        perimeter += 1
-                    if grid[i + 1][j] == 0:
-                        perimeter += 1
-                    if grid[i][j - 1] == 0:
-                        perimeter += 1
-                    if grid[i][j + 1] == 0:
-                        perimeter += 1
+            if grid[i][j] == 1:
+                if grid[i - 1][j] == 0:
+                    perimeter += 1
+                if grid[i + 1][j] == 0:
+                    perimeter += 1
+                if grid[i][j - 1] == 0:
+                    perimeter += 1
+                if grid[i][j + 1] == 0:
+                    perimeter += 1
     return perimeter
