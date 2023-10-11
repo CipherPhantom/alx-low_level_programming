eeaba153d8ada4832f4a39e8ade9fcb2eaae43bb
Cipher Phantom <oyepitan10@gmail.com>
2023-10-11 14:39:07 +0100

    100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -14,7 +14,7 @@ int jump_search(int *array, size_t size, int value)
 {
 	size_t step, start, end, i;
 
-	if (!array || !size)
+	if (array == NULL || size == 0)
 		return (-1);
 
 	step = sqrt((double)size);
