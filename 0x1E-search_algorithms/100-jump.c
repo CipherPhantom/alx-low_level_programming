d30aa2f569dec322eb61c0c43fee62b5c041093e
Cipher Phantom <oyepitan10@gmail.com>
2023-10-11 14:40:09 +0100

    100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -25,7 +25,7 @@ int jump_search(int *array, size_t size, int value)
 			return (end);
 		start = end;
 		end += step;
-	} while (array[end] < value && end < size);
+	} while (array[end] <= value && end < size);
 
 	printf("Value found between indexes [%ld] and [%ld]\n", start, end);
 	for (i = start; i <= end && i < size; i++)
